d1f73e278ed13170a5d32b31933fac455f5243ab
Chaira Santos <chairassacramento@outlook.com>
2022-12-15 15:20:40 -0300

    novo

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$banco = 'projeto_login';
+
+//string de conexão
+$mysqli = new mysqli($servidor,$usuario,$senha,$banco);
+
+
+?>
--- index.php
+++ index.php
@@ -1,53 +1,26 @@
-
 <!doctype html>
 <html lang="pt-br">
-
-<head>
-    <!-- Required meta tags -->
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-</head>
-
-<body>
-    <div class="container">
-        <?php
-            $sql = "SELECT * FROM dados_jogos";
-            $query = $mysqli->query($sql);
-            $i = 1;
-            $j = 2;
-            while ($dados = $query->fetch_array()){
-        ?>
-        <div class="row">
-            <div class="col-12">
-            Data do Jogo: <?php echo $dados['data']; ?> - 
-            Hora do Jogo: <?php echo $dados['horario']; ?> - 
-            Estadio: <?php echo $dados['local']; ?> - <br> 
-            <img src="_images/<?php echo $dados['timea']; ?>.png">
-            <?php echo $dados['timea']; ?>
-            <input type="text" class="form-control" width="20px" name="cp<?php echo $i; ?>"> x 
-            <input type="text" name="cp<?php echo $j; ?>" class="form-control" width="20px">
-            <img src="_images/<?php echo $dados['timeb']; ?>.png"><br>
-            </div>
-            
-        </div>
-        <?php $i++;$j++;} ?>
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/style.css">
+  </head>
+  <body id="fundo">
+  <div id="corpo-form">
+    <h1>Login</h1>
+    <form method="POST">
+        <input type="email" name="email" placeholder="Usuário">
+        <input type="password" name="senha" placeholder="Senha">
+        <input type="submit" value="Entrar" href="home.php">
+        <a href="cadastrar.php">Ainda não é inscrito?<strong>Cadastre-se</strong>
+    </form>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-</body>
-
+    <?php
+    
+    
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
 </html>
